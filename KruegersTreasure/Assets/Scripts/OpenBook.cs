d8aae91b96e8cc56ba5a4d8da8fd333189cb3b63
ksg42069 <lohitksg1295@gmail.com>
2023-03-11 18:52:55 +0530

    book opening script

---
+++ KruegersTreasure/Assets/Scripts/OpenBook.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenBook : MonoBehaviour
+{
+    private Animator animator;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void TriggerOpen()
+    {
+        bool isOpen = animator.GetBool("Book_open");
+
+        animator.SetBool("Book_open", !isOpen);
+
+    }
+}
