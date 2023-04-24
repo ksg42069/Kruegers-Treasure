4a405c696e580df7b7125e0df619caff771b507e
ksg42069 <lohitksg1295@gmail.com>
2023-04-24 09:22:17 +0530

    Main Gate Close animation and sound

---
+++ KruegersTreasure/Assets/Scripts/CloseMainGate.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseMainGate : MonoBehaviour
+{
+    public Animation anim;
+    public AudioSource sound;
+    
+    void Start()
+    {
+        //gate = GetComponent<GameObject>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Body"))
+        {
+            anim.Play();
+            sound.Play();
+        }
+        
+        
+    }
+}
